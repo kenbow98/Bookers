3ab46d3f849763ac4488b53831be0bd18a648ef0
kenbow98 <kenbow.998@gmail.com>
2022-04-15 14:53:56 +0000

    Add controller flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+
+    if @book.save
+     redirect_to book_path(@book.id)
+     flash[:notice] = "Book was successfully created."
+
+
+    else
+      @books = Book.all
+      render :index
+
+    end
   end
 
   def index
@@ -24,15 +33,27 @@ class BooksController < ApplicationController
   end
 
   def update
-   book = Book.find(params[:id])
-   book.update(book_params)
-   redirect_to book_path(book.id)
+   @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+
+    else
+      @books = Book.all
+      render :index
+
+
+    end
+
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
+    flash[:notice] = "Book was successfully destroy."
   end
 
   private
